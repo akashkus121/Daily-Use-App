24b8ec3c3a346dee0d3366f52ce79d3b2266a706
Akash Kushwaha <90akashkushwaha@gmail.com>
2025-08-26 12:00:48 +0530

    Add project files.

---
+++ Daily-Use App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Daily_Use_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
